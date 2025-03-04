4d43e35fcb287184c831b0768823cb201b5b23c4
Zahra Mehri <zahra.m@4D.local>
2025-03-04 09:55:12 +0330

    use redux-persist for set data in localstorage

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './store'
+import { store , persistor } from './store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
 
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 
   // </StrictMode>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import todosReducer from './slices/todosSlice'
 
-export const store = configureStore({
-  reducer: {
-    todos: todosReducer,
-  },
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+  todos: todosReducer
 })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['todos']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer
+})
+
+const persistor = persistStore(store)
+
+export { store, persistor }
+
 
